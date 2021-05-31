835f8027f332e0e86ca5a5a8674433ebfd6ea797
Kirill M <kmatviets@medsoft.su>
2021-05-31 14:13:18 +0300

    Add console info messages

--- internal/main.go
+++ internal/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/TheForgotten69/goinsta/v2"
 	"os"
@@ -12,15 +13,29 @@ type Config struct {
 	TARGETUSER string
 }
 
+type flwType string
+
+const (
+	Followers  flwType = "Followers"
+	Followings flwType = "Followings"
+)
+
+var userFollowers, userFollowings map[string][]goinsta.User
+
 func main() {
 	//https://pkg.go.dev/github.com/TheForgotten69/goinsta/v2@v2.6.0
 
 	config := initConfig()
 	if insta, err := login(config); err == nil {
-		if followers, err := getUserFollowers(config.TARGETUSER, insta, 200); err == nil {
-			for _, follower := range followers {
-				fmt.Println(follower)
-			}
+		if followersList, err := getUserFlws(config.TARGETUSER, Followers, insta, 200); err == nil {
+			userFollowers = make(map[string][]goinsta.User)
+			userFollowers[config.TARGETUSER] = followersList
+		} else {
+			fmt.Println(err)
+		}
+		if followingsList, err := getUserFlws(config.TARGETUSER, Followings, insta, 200); err == nil {
+			userFollowings = make(map[string][]goinsta.User)
+			userFollowings[config.TARGETUSER] = followingsList
 		} else {
 			fmt.Println(err)
 		}
@@ -29,38 +44,30 @@ func main() {
 	}
 }
 
-func login(config Config) (insta *goinsta.Instagram, err error) {
-	if workDir, err := os.Getwd(); err == nil {
-		if insta, err := goinsta.Import(workDir + "/config/LoginSettings.json"); err != nil {
-			insta = goinsta.New(config.USERNAME, config.PASSWORD)
+func getUserFlws(targetUser string, fType flwType, insta *goinsta.Instagram, limit ...int) (flws []goinsta.User, err error) {
+	if searchResult, err := insta.Search.User(targetUser); err == nil {
+		flwUsers := make([]goinsta.User, 0)
+		var flws *goinsta.Users
 
-			if err := insta.Login(); err != nil {
-				return nil, err
+		switch fType {
+		case Followers:
+			flws = searchResult.Users[0].Followers()
+		case Followings:
+			flws = searchResult.Users[0].Following()
+		}
+
+		if flws != nil {
+			for flws.Next() {
+				flwUsers = append(flwUsers, flws.Users...)
+
+				if len(limit) != 0 && len(flwUsers) >= limit[0] {
+					return flwUsers, nil
+				}
 			}
-
-			insta.Export(workDir + "/config/LoginSettings.json")
-			return insta, nil
+			return flwUsers, nil
 		} else {
-			return insta, nil
+			return nil, errors.New(string(fType) + " not found")
 		}
-	} else {
-		return nil, err
-	}
-}
-
-func getUserFollowers(userName string, insta *goinsta.Instagram, limit ...int) (followers []goinsta.User, err error) {
-	if searchResult, err := insta.Search.User(userName); err == nil {
-		followerUsers := make([]goinsta.User, 0)
-		followers := searchResult.Users[0].Followers()
-
-		for followers.Next() {
-			followerUsers = append(followerUsers, followers.Users...)
-
-			if len(limit) != 0 && len(followerUsers) >= limit[0] {
-				return followerUsers, nil
-			}
-		}
-		return followerUsers, nil
 	} else {
 		return nil, err
 	}
@@ -85,6 +92,32 @@ func initConfig() (config Config) {
 	}
 
 	config = Config{USERNAME, PASSWORD, TARGETUSER}
-	fmt.Print("CONFIG INITIALIZED ")
+	fmt.Println("#Config initialized ")
 	return config
+}
+
+func login(config Config) (insta *goinsta.Instagram, err error) {
+	if workDir, err := os.Getwd(); err == nil {
+		if insta, err := goinsta.Import(workDir + "\\config\\LoginSettings.json"); err != nil {
+			insta = goinsta.New(config.USERNAME, config.PASSWORD)
+
+			if err := insta.Login(); err == nil {
+				fmt.Println("#Login successfully")
+				if err := insta.Export(workDir + "\\config\\LoginSettings.json"); err != nil {
+					return nil, err
+				} else {
+					fmt.Println("#Login data export successfully")
+				}
+			} else {
+				return nil, err
+			}
+
+			return insta, nil
+		} else {
+			fmt.Println("#Login data import successfully")
+			return insta, nil
+		}
+	} else {
+		return nil, err
+	}
 }
