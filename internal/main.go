90ee6bb6dc3d6e49e6bc6183ede064c137a264df
Kirill M <kmatviets@medsoft.su>
2021-06-02 16:14:16 +0300

    Refactoring

--- internal/main.go
+++ internal/main.go
@@ -1,94 +1,76 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"github.com/TheForgotten69/goinsta/v2"
 	"os"
 	"time"
 )
 
-type flwType string
-
-const (
-	Followers  flwType = "Followers"
-	Followings flwType = "Followings"
-)
-
-var userFollowers, userFollowings map[string][]goinsta.User
+var config Config
+var insta *goinsta.Instagram
+var targetUsers map[string]*goinsta.User
+var usersFollowers, usersFollowings map[string][]goinsta.User
 
 func main() {
-	//https://pkg.go.dev/github.com/TheForgotten69/goinsta/v2@v2.6.0
 	defer db.Close()
 	initDB()
 
-	config := initConfig()
-	return
-	if insta, err := login(&config); err == nil {
-		if followersList, err := getUserFlws(config.TARGETUSER, Followers, insta, config.REQUEST_DELAY_MIN, config.REQUEST_DELAY_MAX, 200); err == nil {
-			userFollowers = make(map[string][]goinsta.User)
-			userFollowers[config.TARGETUSER] = followersList
-		} else {
-			fmt.Println(err)
-		}
-		if followingsList, err := getUserFlws(config.TARGETUSER, Followings, insta, config.REQUEST_DELAY_MIN, config.REQUEST_DELAY_MAX, 200); err == nil {
-			userFollowings = make(map[string][]goinsta.User)
-			userFollowings[config.TARGETUSER] = followingsList
-		} else {
-			fmt.Println(err)
-		}
+	config = initConfig()
+	//return
+	if ins, err := login(); err == nil {
+		insta = ins
+		usersFollowers = make(map[string][]goinsta.User)
+		usersFollowings = make(map[string][]goinsta.User)
+
+		getUserInfo(config.TARGETUSER)
 	} else {
 		fmt.Println("Login", err)
 	}
 }
 
-func getListsDifference(usersList1, usersList2 []goinsta.User) (diffList []goinsta.User) {
-	usersMap := make(map[int64]struct{}, len(usersList2))
-	for _, user := range usersList2 {
-		usersMap[user.ID] = struct{}{}
-	}
-	for _, user := range usersList1 {
-		if _, found := usersMap[user.ID]; !found {
-			diffList = append(diffList, user)
-		}
-	}
-	return diffList
-}
-
-func getUserFlws(targetUser string, fType flwType, insta *goinsta.Instagram, delayMin int, delayMax int, limit ...int) (flws []goinsta.User, err error) {
-	if searchResult, err := insta.Search.User(targetUser); err == nil {
-		flwUsers := make([]goinsta.User, 0)
-		var flws *goinsta.Users
-
-		switch fType {
-		case Followers:
-			flws = searchResult.Users[0].Followers()
-		case Followings:
-			flws = searchResult.Users[0].Following()
-		}
-
-		if flws != nil {
-			for flws.Next() {
-				flwUsers = append(flwUsers, flws.Users...)
-
-				delay := getRandomNumber(delayMin-getRandomNumber(0, 200), delayMax+getRandomNumber(0, 500))
-				time.Sleep(time.Duration(delay) * time.Millisecond)
-				fmt.Printf("Delay: %v, %v users: %v \n", delay, fType, len(flwUsers))
-
-				if len(limit) != 0 && len(flwUsers) >= limit[0] {
-					return flwUsers, nil
-				}
-			}
-			return flwUsers, nil
-		} else {
-			return nil, errors.New(string(fType) + " not found")
-		}
+func getUserInfo(targetUserName string) {
+	if targetUserInfo, err := insta.Profiles.ByName(targetUserName); err == nil {
+		targetUsers[targetUserName] = targetUserInfo
 	} else {
-		return nil, err
+		fmt.Println(err)
 	}
 }
 
-func login(config *Config) (insta *goinsta.Instagram, err error) {
+func getUserFollowers(targetUserName string){
+	if followersList, err := getUserFlws(targetUsers[targetUserName].Followers(), 200); err == nil {
+		usersFollowers[targetUserName] = followersList
+	} else {
+		fmt.Println(err)
+	}
+}
+
+func getUserFollowings(targetUserName string)  {
+	if followingsList, err := getUserFlws(targetUsers[targetUserName].Following(), 200); err == nil {
+		usersFollowings[targetUserName] = followingsList
+	} else {
+		fmt.Println(err)
+	}
+}
+
+func getUserFlws(users *goinsta.Users, limitFlwsCount ...int) (flwUsers []goinsta.User, err error) {
+	flwUsers = make([]goinsta.User, 0)
+
+	for users.Next() {
+		flwUsers = append(flwUsers, users.Users...)
+
+		delay := getRandomNumber(config.REQUEST_DELAY_MIN-getRandomNumber(0, 200), config.REQUEST_DELAY_MAX+getRandomNumber(0, 500))
+		time.Sleep(time.Duration(delay) * time.Millisecond)
+		fmt.Printf("Delay: %v; users: %v \n", delay, len(flwUsers))
+
+		if len(limitFlwsCount) != 0 && len(flwUsers) >= limitFlwsCount[0] {
+			return flwUsers, nil
+		}
+	}
+	return flwUsers, nil
+}
+
+func login() (insta *goinsta.Instagram, err error) {
 	if workDir, err := os.Getwd(); err == nil {
 		if insta, err := goinsta.Import(workDir + "\\accounts\\" + config.USERNAME + ".json"); err != nil {
 			insta = goinsta.New(config.USERNAME, config.PASSWORD)
@@ -112,4 +94,17 @@ func login(config *Config) (insta *goinsta.Instagram, err error) {
 	} else {
 		return nil, err
 	}
+}
+
+func getListsDifference(usersList1, usersList2 []goinsta.User) (diffList []goinsta.User) {
+	usersMap := make(map[int64]struct{}, len(usersList2))
+	for _, user := range usersList2 {
+		usersMap[user.ID] = struct{}{}
+	}
+	for _, user := range usersList1 {
+		if _, found := usersMap[user.ID]; !found {
+			diffList = append(diffList, user)
+		}
+	}
+	return diffList
 }
