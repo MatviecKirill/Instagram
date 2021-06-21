e6723f8e1acbbfadfb3b7a20a4740714e4349e85
Kirill M <kmatviets@medsoft.su>
2021-06-21 15:45:17 +0300

    Telegram webhook reset

--- internal/Telegram/telegram.go
+++ internal/Telegram/telegram.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"os"
+	"strconv"
+	"time"
 )
 
 var bot *tgbotapi.BotAPI
@@ -20,26 +22,31 @@ func Init(token string, messageChannel chan tgbotapi.Message) {
 	//Телеграм бот запускается с использованием webhook'a
 	//Когда кто-то напишет боту, он обратится к моему серверу
 	//heroku labs:enable runtime-dyno-metadata -a <app name> - включить метаданные для приложения. HEROKU_APP_NAME
-	if _, err := bot.SetWebhook(tgbotapi.NewWebhook("https://" + os.Getenv("HEROKU_APP_NAME") + ".herokuapp.com/" + bot.Token)); err == nil {
-		if info, err := bot.GetWebhookInfo(); err == nil {
-			if info.LastErrorDate == 0 {
-				updates := bot.ListenForWebhook("/" + bot.Token)
-				for update = range updates {
-					if update.Message == nil {
-						continue
+	for attemptCount := 0; attemptCount <= 10; attemptCount++ {
+		if _, err := bot.SetWebhook(tgbotapi.NewWebhook("https://" + os.Getenv("HEROKU_APP_NAME") + ".herokuapp.com/" + bot.Token)); err == nil {
+			if info, err := bot.GetWebhookInfo(); err == nil {
+				if info.LastErrorDate == 0 {
+					updates := bot.ListenForWebhook("/" + bot.Token)
+					for update = range updates {
+						if update.Message == nil {
+							continue
+						}
+						fmt.Printf("Message from [%s]: %s\n", update.Message.From.FirstName, update.Message.Text)
+						messageChannel <- *update.Message
 					}
-					fmt.Printf("Message from [%s]: %s\n", update.Message.From.FirstName, update.Message.Text)
-					messageChannel <- *update.Message
-				}
 
+				} else {
+					fmt.Printf("Telegram callback failed: %s\n", info.LastErrorMessage)
+					fmt.Println("Attempt count: " + strconv.Itoa(attemptCount+1))
+					time.Sleep(time.Second * 1)
+					bot.SetWebhook(tgbotapi.NewWebhook("http://" + os.Getenv("HEROKU_APP_NAME") + ".herokuapp.com/" + bot.Token))
+				}
 			} else {
-				fmt.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+				fmt.Println(err)
 			}
 		} else {
 			fmt.Println(err)
 		}
-	} else {
-		fmt.Println(err)
 	}
 }
 
@@ -47,4 +54,8 @@ func SendMessage(message string) {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, message)
 
 	bot.Send(msg)
+}
+
+func CreateKeyboard() {
+	tgbotapi.NewKeyboardButton("1")
 }
