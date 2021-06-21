719a85cd866209b9793060bfb0f42ef58127baa0
Leif <matviec.ka@gmail.com>
2021-06-21 23:33:57 +0300

    Telegram webhook reset

--- internal/Telegram/telegram.go
+++ internal/Telegram/telegram.go
@@ -22,18 +22,22 @@ func Init(token string, messageChannel chan tgbotapi.Message) {
 	//Телеграм бот запускается с использованием webhook'a
 	//Когда кто-то напишет боту, он обратится к моему серверу
 	//heroku labs:enable runtime-dyno-metadata -a <app name> - включить метаданные для приложения. HEROKU_APP_NAME
+	setWebhook()
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	for update = range updates {
+		if update.Message == nil {
+			continue
+		}
+		fmt.Printf("Message from [%s]: %s\n", update.Message.From.FirstName, update.Message.Text)
+		messageChannel <- *update.Message
+	}
+}
+
+func setWebhook() {
 	for attemptCount := 0; attemptCount <= 10; attemptCount++ {
 		if _, err := bot.SetWebhook(tgbotapi.NewWebhook("https://" + os.Getenv("HEROKU_APP_NAME") + ".herokuapp.com/" + bot.Token)); err == nil {
 			if info, err := bot.GetWebhookInfo(); err == nil {
 				if info.LastErrorDate == 0 {
-					updates := bot.ListenForWebhook("/" + bot.Token)
-					for update = range updates {
-						if update.Message == nil {
-							continue
-						}
-						fmt.Printf("Message from [%s]: %s\n", update.Message.From.FirstName, update.Message.Text)
-						messageChannel <- *update.Message
-					}
 
 				} else {
 					fmt.Printf("Telegram callback failed: %s\n", info.LastErrorMessage)
